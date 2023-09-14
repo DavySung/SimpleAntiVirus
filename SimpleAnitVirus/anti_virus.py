88c7ec79d20c93a94796efaf9b414fccd0bba8a1
Davy Sung <103535521@student.swin.edu.au>
2023-09-14 15:46:29 +1000

    Initial Commit for file hash function

--- SimpleAnitVirus/anti_virus.py
+++ SimpleAnitVirus/anti_virus.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import customtkinter
-import psutil
-import subprocess
+from filehash import HashFile
+from tkinter import Label, StringVar, filedialog
+
 
 customtkinter.set_appearance_mode("light")
 class App(customtkinter.CTk):
@@ -19,8 +20,8 @@ class App(customtkinter.CTk):
         self.button2 = customtkinter.CTkButton(self, text="File Scan", command=self.button_callback)
         self.button2.grid(row=1, column=2, padx=5, pady=20, sticky="ew")
 
-        self.button3 = customtkinter.CTkButton(self, text="File Hash", command=self.button_callback)
-        self.button3.grid(row=1, column=3, padx=5, pady=10, sticky="ew")
+        self.fileHashBtn = customtkinter.CTkButton(self, text="File Hash", command=self.hashWindow)
+        self.fileHashBtn.grid(row=1, column=3, padx=5, pady=10, sticky="ew")
         
         self.button4 = customtkinter.CTkButton(self, text="Quarantine", command=self.button_callback)
         self.button4.grid(row=1, column=4, padx=5, pady=10, sticky="ew")
@@ -30,6 +31,85 @@ class App(customtkinter.CTk):
 
     def button_callback(self):
         print("button pressed")
+
+    def goBack(self):
+        self.destroy()
+
+    def open_file_dialog(self, file_path, sha256):
+        self.filename = filedialog.askopenfilename()
+        file_path.set(self.filename)
+        if file_path:
+            file = HashFile()
+            file.fileName = self.filename
+            output = file.hash_file() 
+            sha256.set(output)
+
+    def hashWindow(self):
+        new_window = customtkinter.CTkToplevel(self)
+        file = HashFile()
+
+        f1 = StringVar()
+        f2 = StringVar()
+        Sha256f1 = StringVar()
+        Sha256f2 = StringVar()
+        # MD5f1 = StringVar()
+        # MD5f2 = StringVar()
+        # Sha1f1 = StringVar()
+        # Sha1f2 = StringVar()
+
+        new_window.title("HashFile")
+        new_window.geometry("1000x600")
+        new_window.grid_columnconfigure((0,1,2,3,4,5,6), weight=1)
+        new_window.grid_rowconfigure((0, 1,2,3,4, 5,6), weight=1)
+
+        #hashfile1
+        new_window.f1Lbl = Label(new_window, textvariable=f1, width=60, anchor=W)
+        new_window.f1Lbl.grid(row=0, column=0, padx=5, pady=10, sticky="ew")
+        f1.set("First file or enter a HASH below.")
+
+        new_window.f1HashLbl3 = customtkinter.CTkLabel(new_window, text="Sha256 : ", width=10, anchor=W)
+        new_window.f1HashLbl3.grid(row=1, column=0, padx=5, pady=10, sticky="ew")
+
+        new_window.f1Sha256 = customtkinter.CTkEntry(new_window, textvariable=Sha256f1, width=100)
+        new_window.f1Sha256.grid(row=1, column=1, padx=5, pady=10, sticky="ew")
+
+        new_window.openF1 = customtkinter.CTkButton(new_window, text="Open File 1", command=lambda: self.open_file_dialog( f1, Sha256f1))
+        new_window.openF1.grid(row=1, column=3, padx=5, pady=10, sticky="ew")
+       
+        #hashfile2
+        new_window.f2Lbl = Label(new_window, textvariable=f2, width=60, anchor=W)
+        new_window.f2Lbl.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
+        f2.set("Second file or enter a HASH below.")
+
+        new_window.f2HashLbl3 = customtkinter.CTkLabel(new_window, text="Sha256 : ")
+        new_window.f2HashLbl3.grid(row=3, column=0, padx=5, pady=10, sticky="ew")
+
+        new_window.f2Sha256 = customtkinter.CTkEntry(new_window, textvariable=Sha256f2, width=100)
+        new_window.f2Sha256.grid(row=3, column=1, padx=5, pady=10, sticky="ew")
+
+        new_window.openF2 = customtkinter.CTkButton(new_window, text="Open File 2", command=lambda: self.open_file_dialog( f2, Sha256f2))
+        new_window.openF2.grid(row=3, column=3, padx=5, pady=10, sticky="ew")
+       
+        #Hash Function
+        new_window.checkHash = customtkinter.CTkButton(new_window, text="Compare Two Hash", command=lambda: file.CheckHash(new_window.f1Sha256.get(),new_window.f2Sha256.get()))
+        new_window.checkHash.grid(row=4, column=0, padx=5, pady=5, sticky="ew")
+
+        new_window.storeMalicious = customtkinter.CTkButton(new_window, text="Store Malicious", command=lambda: file.StoreFile("malicious_file.txt", new_window.f1Sha256.get()))
+        new_window.storeMalicious.grid(row=4, column=1, padx=5, pady=10, sticky="ew")
+
+        new_window.storeTrusted = customtkinter.CTkButton(new_window, text="Store Trusted", command=lambda: file.StoreFile("trusted_file.txt", new_window.f1Sha256.get()))
+        new_window.storeTrusted.grid(row=4, column=2, padx=5, pady=10, sticky="ew")
+
+        new_window.checkMalicious = customtkinter.CTkButton(new_window, text="Check Malicious", command=lambda: file.CheckFile("malicious_file.txt", new_window.f1Sha256.get()))
+        new_window.checkMalicious.grid(row=4, column=3, padx=5, pady=10, sticky="ew")
+
+        new_window.checkTrusted = customtkinter.CTkButton(new_window, text="Check Trusted", command=lambda: file.CheckFile("trusted_file.txt", new_window.f1Sha256.get()))
+        new_window.checkTrusted.grid(row=4, column=4, padx=5, pady=10, sticky="ew")
+
+        new_window.back = customtkinter.CTkButton(new_window, text="Go Back", command=self.goBack)
+        new_window.back.grid(row=4, column=5, padx=5, pady=10, sticky="ew")
+
+        self.withdraw()
 
 app = App()
 app.mainloop()
---
+++ SimpleAnitVirus/filehash.py
@@ -0,0 +1,80 @@
+import hashlib
+
+# HashedFile: HashLib
+# Hash(FilePath: string)
+# CheckHash(Hash)
+#This function check file hash integrity
+
+# hash file content
+# check file if it has been modified or corrupted
+#Compare the file hash of a given file with a previous hash file copy of the same file
+# And also allow user custom input hash files as malicious or to be trusted
+
+#Integrity check for sensitive file 
+
+class HashFile:
+
+    def _init_(self, fileName):
+        self.fileName = fileName
+
+    def hash_file(self):
+        hash_obj = hashlib.sha256()
+        with open(self.fileName, 'rb') as file:
+            while True:
+                chunk = file.read(4096)
+                if not chunk:
+                    break
+                hash_obj.update(chunk)
+        return hash_obj.hexdigest()
+
+    def CheckHash(self, output1, output2):
+        if(output1 == output2):
+            print("Both Hash is the same", output1, output2)
+        else:
+            print("Different Hash")
+            
+
+    def StoreFile(self, fileName, text):
+        flag = False
+        with open(fileName, "a+") as file:
+            if(os.stat(fileName).st_size == 0):
+                file.write(text)
+                file.write("\n")
+                print("File is stored")
+            else:
+                with open(fileName, "r") as file1:
+                    Lines = file1.readlines()
+                    for line in Lines:
+                        if(text == line.strip()):
+                            print("File existed")
+                            flag = False
+                            break
+                        else:
+                            flag = True
+                    if(flag == True):
+                        file.write(text)
+                        file.write("\n")
+                        print("File is stored")
+
+    def CheckFile(self, fileName, text):
+        flag = False
+        output = ''
+        with open(fileName, "r") as file1:
+            Lines = file1.readlines()
+            for line in Lines:
+                print(line)
+                if(text == line.strip()):
+                    if(fileName == "malicious_file.txt"):
+                        flag = True
+                        output += 'File is corrupted'
+                        break
+                    if(fileName == "trusted_file.txt"):
+                        output += 'File is trusted'
+                        flag = True
+                        break
+                else: 
+                    flag = False
+            if(flag == False):
+                print("File not found")
+            else:
+                print(output)
