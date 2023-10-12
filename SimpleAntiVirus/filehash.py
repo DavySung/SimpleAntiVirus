98bf90e8ad2032bced5acbc5fa20831bf767110a
Reed Panozza <101631919@student.swin.edu.au>
2023-10-12 19:13:06 +1100

    Added error exceptions to hash_file method

--- SimpleAntiVirus/filehash.py
+++ SimpleAntiVirus/filehash.py
@@ -18,6 +18,7 @@ class HashFile:
         self.fileName = fileName
 
     def hash_file(self, filename, hash_obj):
+<<<<<<< Updated upstream
         with open(filename, 'rb') as file:
             while True:
                 chunk = file.read(4096)
@@ -25,6 +26,24 @@ class HashFile:
                     break
                 hash_obj.update(chunk)
         return hash_obj.hexdigest()
+=======
+        try:
+            with open(filename, 'rb') as file:
+                while True:
+                    chunk = file.read(4096)
+                    if not chunk:
+                        break
+                    hash_obj.update(chunk)
+            return hash_obj.hexdigest()
+        except PermissionError:
+            # This occurs when SAV does not have adequate permissions and tries to scan a protected file.
+            return ""
+        except FileNotFoundError:
+            # This needs investigating,
+            # suspect is temporary files that no longer exist when the scan function gets around to scanning it
+            # 
+            return ""
+>>>>>>> Stashed changes
 
     def CheckHash(self, sha256Output1, sha256Output2,md5Output1, md5Output2, sha1Output1, sha1Output2):
         try:
