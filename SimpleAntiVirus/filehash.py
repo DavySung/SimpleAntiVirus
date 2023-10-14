68a1201939ab531508b3b315adab453192af4e50
Reed Panozza <101631919@student.swin.edu.au>
2023-10-14 21:37:00 +1100

    Added raised exception for missing file in file_hash function

--- SimpleAntiVirus/filehash.py
+++ SimpleAntiVirus/filehash.py
@@ -28,6 +28,9 @@ class HashFile:
             return hash_obj.hexdigest()
         except PermissionError:
             return ""
+        except FileNotFoundError:
+            return ""
+
 
     def CheckHash(self, sha256Output1, sha256Output2,md5Output1, md5Output2, sha1Output1, sha1Output2):
         try:
